a6b3e1c9fc09b77244bef35488994ad747170f4e
Roberto Ciano <roberto@second-page.de>
2017-04-25 12:48:33 +0200

    fix button labels

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ export default class EUFuckingLaw {
     this.cookieAcceptBar.className = 'eufuckingcookie-acceptbar'
 
     const infoTextElement = document.createElement('p')
-    infoTextElement.innerText = `
-      Diese Seite verwendet Cookies. NAch EU-Richtlinien sind wir verpflichtet, Ihnen dies mitzuteilen und Ihnen die Möglichkeit zu gewähren,
+    infoTextElement.innerText = `Diese Seite verwendet Cookies. NAch EU-Richtlinien sind wir verpflichtet, Ihnen dies mitzuteilen und Ihnen die Möglichkeit zu gewähren,
       Cookies für diese Seite zu deaktivieren. Bitte beachten Sie jedoch, dass mit der Ablehnung der Cookies der Funktionsumfang der Seite
       unter Umständen eingeschränkt wird.
     `
@@ -28,13 +27,13 @@ export default class EUFuckingLaw {
     const rejectButton = document.createElement('button')
     rejectButton.className = 'eufuckingcookie-declinebutton'
     rejectButton.onclick = this.rejectCookies
-    rejectButton.value = 'Decline'
+    rejectButton.innerText = 'Decline'
     this.cookieAcceptBar.appendChild(rejectButton)
 
     const acceptButton = document.createElement('button')
     acceptButton.className = 'eufuckingcookie-acceptbutton'
     acceptButton.onclick = this.acceptCookies
-    acceptButton.value = 'Accept'
+    acceptButton.innerText = 'Accept'
     this.cookieAcceptBar.appendChild(acceptButton)
 
     this.parent.appendChild(this.cookieAcceptBar)
