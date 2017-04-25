4cdbff4887b1377ab06f972d66c2081f2aac781a
Roberto Ciano <roberto@second-page.de>
2017-04-25 11:42:50 +0200

    add basic webpack workflow

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
